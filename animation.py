2ebbe040a0834bbd643a0d137d293da0994a3eb7
E-BAO <714667117@qq.com>
2017-08-25 09:56:01 -0600

    set animation

--- animation.py
+++ animation.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as manimation
 import cv2  
 
-filename = "replacement_normalize.txt"
+filename = "EQ7/Calibration/Calibrate_Noise3_Inch.txt"
 
 X0 = []
 X1 = []
@@ -23,15 +23,15 @@ with open(filename, "r") as f:
         break
         pass
     line = line[:line.find('\t\n')]
-    frame_i, x0, y0, x1, y1, x2, y2, x3, y3 = [float(i) for i in line.split('\t')]
+    frame_i, x0, y0, x1, y1, x2, y2 = [float(i) for i in line.split('\t')]
     X0.append(x0)
     X1.append(x1)
     X2.append(x2)
-    X3.append(x3)
+    # X3.append(x3)
     Y0.append(y0)
     Y1.append(y1)
     Y2.append(y2)
-    Y3.append(y3)
+    # Y3.append(y3)
     pass
 
 # first set up the figure, the axis, and the plot element we want to animate   
@@ -56,44 +56,55 @@ fig = plt.figure()
 
 fig.suptitle('Video Data Analysis of Roof Level', fontsize=12)
 
-ax1 = fig.add_subplot(2,2,1,xlim=(1, len(X0)), ylim=(-30, 50)) 
+ax1 = fig.add_subplot(2,2,1,xlim=(0, len(X0)/30.), ylim=(-30, 50)) 
 ax1.set_title('X Relative Displacement',fontweight="bold", size=6) # Title
-ax1.set_ylabel('Deplacement(pixel)', fontsize = 6) # Y label
-ax1.set_xlabel('time(frame)', fontsize = 6) # X label
+ax1.set_ylabel('Deplacement(inch)', fontsize = 6) # Y label
+ax1.set_xlabel('time(sec)', fontsize = 6) # X label
 
-line0, = ax1.plot([], [], lw=1,label='TopLeft')   
-line1, = ax1.plot([], [], lw=1,label='TopRight')   
-line2, = ax1.plot([], [], lw=1,label='BottomRight')   
-line3, = ax1.plot([], [], lw=1,label='BottomLeft')    
+line0, = ax1.plot([], [], lw=1,label='BottomLeft',color = (0,0,1))   
+# line3, = ax1.plot([], [], lw=1,label='BottomRight', color = (0.8,0.8,0))    
+line1, = ax1.plot([], [], lw=1,label='TopLeft',color = (1,0,0))   
+line2, = ax1.plot([], [], lw=1,label='TopRight',color = (0,1,0))   
 ax1.legend(loc='upper right')
-text1 = ax1.text(80, 40, str(i), fontsize=6)
+# text1 = ax1.text(80, 60, str(i), fontsize=6)
 
 
-ax2 = fig.add_subplot(2,2,2,xlim=(1, len(X0)), ylim=(-30, 50))
+ax2 = fig.add_subplot(2,2,2,xlim=(0, len(X0)/30.), ylim=(-30, 50))
 ax2.set_title('Y Relative Displacement',fontweight="bold", size=6) # Title
-ax2.set_ylabel('Deplacement(pixel)', fontsize = 6) # Y label
-ax2.set_xlabel('time(frame)', fontsize = 6) # X label
+ax2.set_ylabel('Deplacement(inch)', fontsize = 6) # Y label
+ax2.set_xlabel('time(sec)', fontsize = 6) # X label
 
-line4, = ax2.plot([], [], lw=1,label='TopLeft')   
-line5, = ax2.plot([], [], lw=1,label='TopRight')   
-line6, = ax2.plot([], [], lw=1,label='BottomRight')   
-line7, = ax2.plot([], [], lw=1,label='BottomLeft')    
+line4, = ax2.plot([], [], lw=1,label='BottomLeft',color = (0,0,1))   
+# line7, = ax2.plot([], [], lw=1,label='BottomRight', color = (0.8,0.8,0))    
+line5, = ax2.plot([], [], lw=1,label='TopLeft',color = (1,0,0))   
+line6, = ax2.plot([], [], lw=1,label='TopRight',color = (0,1,0))   
 ax2.legend(loc='upper right')
-text2 = ax2.text(80, 40, str(i), fontsize=6)
+# text2 = ax2.text(80, 40, str(i), fontsize=6)
 
-frame_i = 720
+frame_i = 3267
 
-frame = cv2.imread("Preprocess2/frame720.png")
+# cap1 = cv2.VideoCapture("EQ7/OFCneter1.avi")
+frame1 = cv2.imread("EQ7/CalibrationDenoise3t/frame3267.png")
+# ret, frame1 = cap1.read()
+r, c, a = frame1.shape
+# frame1 = frame1[220:r -120,150:c - 200,:]
+
+cap0 = cv2.VideoCapture("EQ7/EQ7_Top.avi")
+
+# for i in range(3266):
+ret, frame0 = cap0.read()
+
+# frame0 = cv2.imread("EQ7/Frames/frame3267.png")
 
 im00 = fig.add_subplot(2,2,3,title = "Original Video")#xlim=(0, 3840), ylim=(0, 2160)
 im00.axis('off')
 im00.set_title('Original',fontweight="bold", size=6) # Title
-im0 = plt.imshow(frame, cmap=plt.get_cmap('jet'), vmin=0, vmax=855)
+im0 = plt.imshow(frame0, cmap=plt.get_cmap('jet'), vmin=0, vmax=855)
 
 im11 = fig.add_subplot(2,2,4,title = "Processed Video")#xlim=(0, 1500), ylim=(0, 970),
 im11.axis('off')
-im11.set_title('Processed',fontweight="bold", size=6) # Title
-im1 = plt.imshow(frame, cmap=plt.get_cmap('jet'), vmin=0, vmax=855)
+im11.set_title('Processed Video',fontweight="bold", size=6) # Title
+im1 = plt.imshow(frame1, cmap=plt.get_cmap('jet'), vmin=0, vmax=855)
 
 plt.tight_layout(pad=10.08, w_pad=3.0, h_pad=3.0)
 
@@ -126,7 +137,6 @@ plt.tight_layout(pad=10.08, w_pad=3.0, h_pad=3.0)
 #     y[i:] = [0 for k in range(n)]
 #     line3.set_data(x, y)   
 
-
 #     frame_i = 720 + i
 #     print frame_i
 #     frame = cv2.imread("Preprocess2/frame%d.png"%(frame_i))
@@ -135,22 +145,20 @@ plt.tight_layout(pad=10.08, w_pad=3.0, h_pad=3.0)
   
 # anim1=animation.FuncAnimation(fig, animate, init_func=init,  frames=len(X0), interval=30)  
 
+# cap1 = cv2.VideoCapture('EQ7/OFCneter_denoise.avi')
 
-cap0 = cv2.VideoCapture("EQ5_Top.MOV")
+# centerX, centerY = 800,460
 
-for i in range(720):
-    ret, frame0 = cap0.read()
+with writer.saving(fig, "EQ7/Calibration/OFGPScali3.mp4", len(X0)):
+    x = np.linspace(0, len(X0)/30., len(X0))
 
-cap1 = cv2.VideoCapture('VideoOpticalFlow_04.avi')
-
-centerX, centerY = 800,460
-
-with writer.saving(fig, "writet.mp4", len(X0)):
-    for i in range(len(X0)):
-        text1.set_text(str(X0[i]))
-        text2.set_text(str(Y0[i]))
-
-        x = np.linspace(1, len(X0), len(X0))
+    for i in range(len(X0)):    
+        # for i in range(len(X0)):
+            # text1.set_text(str(X0[i0]))
+            # text2.set_text(str(Y0[i]))
+            
+        # xMax = len(X0)
+        # xN = len(X0)
         temp = [X0[0] for k in range(len(X0))]
         y = [X0[k] - temp[k] for k in range(len(X0))]
         n = len(X0) - i
@@ -167,10 +175,10 @@ with writer.saving(fig, "writet.mp4", len(X0)):
         y[i:] = [0 for k in range(n)]
         line2.set_data(x, y)   
 
-        temp = [X3[0] for k in range(len(X0))]
-        y = [X3[k] - temp[k] for k in range(len(X0))]
-        y[i:] = [0 for k in range(n)]
-        line3.set_data(x, y)   
+        # temp = [X3[0] for k in range(len(X0))]
+        # y = [X3[k] - temp[k] for k in range(len(X0))]
+        # y[i:] = [0 for k in range(n)]
+        # line3.set_data(x, y)   
 
         temp = [Y0[0] for k in range(len(X0))]
         y = [Y0[k] - temp[k] for k in range(len(X0))]
@@ -187,27 +195,35 @@ with writer.saving(fig, "writet.mp4", len(X0)):
         y[i:] = [0 for k in range(n)]
         line6.set_data(x, y)   
 
-        temp = [Y3[0] for k in range(len(X0))]
-        y = [Y3[k] - temp[k] for k in range(len(X0))]
-        y[i:] = [0 for k in range(n)]
-        line7.set_data(x, y)  
+        # temp = [Y3[0] for k in range(len(X0))]
+        # y = [Y3[k] - temp[k] for k in range(len(X0))]
+        # y[i:] = [0 for k in range(n)]
+        # line7.set_data(x, y)  
 
-        frame_i = 720 + i
+        frame_i = 3267 + i
         print frame_i
-        ret, frame1 = cap1.read()
+        frame1 = cv2.imread("EQ7/CalibrationDenoise3t/frame%d.png"%(frame_i))
+        # ret, frame1 = cap1.read()
         if frame1 is None:
             break
+        frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
+        # frame1 = frame1[220:r -120,150:c - 200,:]
 
         ret, frame0 = cap0.read()
+        # frame = cv2.imread("EQ7/Frames/frame%d.png"%(frame_i))
         frame0 = cv2.cvtColor(frame0, cv2.COLOR_BGR2RGB)
-        # frame = cv2.imread("Preprocess2/frame%d.png"%(frame_i))
         im0.set_array(frame0)
         im1.set_array(frame1)
+
+        # plt.tight_layout(pad=3.08, h_pad=2.0)
+
+        # plt.show()
+
 
         writer.grab_frame()
     # return (line0,line1,line2,line3) ,[im]  
 
-cap.release()
+# cap.release()
 # fig = plt.figure() # make figure
 
 # # make axesimage object
